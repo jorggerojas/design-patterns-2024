aef9bc10203c7e8fe9518b66a5873ff35a655e79
JavierCT05 <161969862+JavierCT05@users.noreply.github.com>
2024-03-05 23:35:57 -0600

    Add files via upload

---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+const roundUp = 1.5
+const rounded = Math.round(1.5)
+console.log(rounded)
--- string-length.js
+++ string-length.js
@@ -1,3 +1,2 @@
-const example = 'example string'
-
-console.log(example.length)
+const example = 'example string';
+console.log(example.length);
