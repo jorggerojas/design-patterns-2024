af576e283f649c72720ee00b056b6761707aec38
DiegoM011 <161967946+DiegoM011@users.noreply.github.com>
2024-03-05 23:48:45 -0600

    Add files via upload

---
+++ Entregable N.- 1/numbers.js
@@ -0,0 +1,5 @@
+// Declare the variable named example
+let example = 123456789;
+
+// Print the number to the console
+console.log(example); 
---
+++ Entregable N.- 1/rounding-numbers.js
@@ -0,0 +1,8 @@
+// Define the variable with the floating-point value
+let roundUp = 1.5;
+
+// Round the number up using Math.round()
+let rounded = Math.round(roundUp);
+
+// Print the rounded number to the console
+console.log(rounded); 
