b94d0cf0afa03013683c9a0336bd5e671c2ffe17
RyosukeIshiwatari <152324377+RyosukeIshiwatari@users.noreply.github.com>
2024-03-08 21:25:20 -0600

    javascripting part 2

---
+++ if-stament.js
@@ -0,0 +1,7 @@
+let fruit = "orange"
+
+if(fruit > 5){
+    console.log('The fruit orange has more than five characters.')
+}else{
+    console.log('The fruit name has more than five characters.')
+}
