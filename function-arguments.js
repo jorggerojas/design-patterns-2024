25f184217c354e600025e5b30b263aa888da68ae
POXTRZ <148813649+POXTRZ@users.noreply.github.com>
2024-03-08 23:02:10 -0600

    Add files via upload
    js

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math(argumento1,argumento2,argumento3) {
+    return argumento1 + (argumento2 * argumento3);
+}
+console.log(math(53,61,67));
--- object-keys.js
+++ object-keys.js
@@ -1,11 +1,8 @@
-const String = {
-  make: 'Honda',
-  model: 'Accord',
-  year: 2020,
-}
-
-
-
-const keys = Object.keys(car)
-
-console.log(keys)
+const car = {
+    make: 'Honda',
+    model: 'Accord',
+    year: 2020
+  }
+  
+  const keys = Object.keys(car);
+  console.log(keys);
