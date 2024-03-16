d5d95bdfad891e8be702ed7e0124e1aad55ecedf
Karen Cervantes Álvarez <karencervantes@MacBook-Air-de-Karen.local>
2024-03-16 08:46:13 -0600

    clase2/ejercicio16

--- claseDos/index.js
+++ claseDos/index.js
@@ -2,7 +2,7 @@ import { generate } from "random-words";
 
 const palabras = generate(50)
 const resultado = palabras.map((palabra) => {
-    const objeto = { [palabra] : palabra.toUpperCase() }
+    const objeto = { [palabra] : palabra.length }
 
     return objeto
 })
