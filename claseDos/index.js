3d86dea6e1f096533ec545227387728dd67faf0f
Karen Cervantes Álvarez <karencervantes@MacBook-Air-de-Karen.local>
2024-03-15 21:40:30 -0600

    clase2/ejercicio3

--- claseDos/index.js
+++ claseDos/index.js
@@ -1,5 +1,12 @@
 import { generate } from "random-words";
 
 const palabras = generate(20);
+const filtradas = palabras.filter(palabra => {
+    if(palabra.includes('s')||palabra.includes('r')){
+        return false
+    }else{
+        return true
+    }
+})
 
-palabras.forEach(palabra => console.log(palabra))
+console.log(filtradas)
