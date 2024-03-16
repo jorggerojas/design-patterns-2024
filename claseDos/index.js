14e94ccf8bf7599a418691b6c361bb9cff93c429
Karen Cervantes Álvarez <karencervantes@MacBook-Air-de-Karen.local>
2024-03-16 09:12:12 -0600

    clase2/ejercicio18

--- claseDos/index.js
+++ claseDos/index.js
@@ -1,11 +1,14 @@
 import { generate } from "random-words";
 
 const palabras = generate(50)
-const filtradas = palabras.filter((elemento)=>elemento.length>=6)
-const resultado = filtradas.map((palabra) => {
-    const objeto = { [palabra] : palabra.length }
-
-    return objeto
+var resultado = palabras.map(function(obj){
+    if(obj.length === 4){
+        var rObj = 'HOLA'
+    }else if(obj.length>4){
+        var rObj = 'MUNDO'
+    }else{
+        var rObj = 'ADIOS'
+    }
+    return rObj;
 })
-
-console.log(resultado)
+console.log (resultado)
