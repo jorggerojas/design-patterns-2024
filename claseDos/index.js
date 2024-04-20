ad2b0fbf7e67290f933415dda2e137cf333b90e1
Karen Cervantes Álvarez <karencervantes@MacBook-Air-de-Karen.local>
2024-04-19 20:38:13 -0600

    claseDos

--- claseDos/index.js
+++ claseDos/index.js
@@ -1,9 +1,10 @@
 import { generate } from "random-words";
 
 const palabras = generate(50)
-let resultado = []
-palabras.forEach((palabra) => {
-    resultado.push(palabra.length)
-})
 console.log(palabras)
-console.log(resultado)
+
+const resultado = palabras.map((palabra) => {
+    const objeto = { [palabra] : palabra.length }
+
+    return objeto
+})
