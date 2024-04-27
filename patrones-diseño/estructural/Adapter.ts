b34f0a98788e4f982f65d028e95528c1c75f635f
nath.dssb <nathalia.dssb@gmail.com>
2024-04-27 12:23:02 -0600

    clase 06: patrones estructurales adapter && bridge

---
+++ patrones-diseño/estructural/Adapter.ts
@@ -0,0 +1,40 @@
+class Target {
+    public request(): string {
+        return 'Targe: Default Targe.';
+    }
+}
+
+class Adaptee {
+    public specificRequest(): string {
+        return 'sodot a aloH';
+    }
+}
+
+class Adapter extends Target {
+
+    constructor( private adaptee: Adaptee) {
+        super();
+        this.adaptee = adaptee;
+    }
+
+    public request(): string {
+        const result = this.adaptee.specificRequest();
+
+        const resultAdapted = result.split('').reverse().join('');
+        
+        return `Adapter: (Traduccion) ${resultAdapted};`;
+    }
+}
+
+function clientCode(target: Target) {
+    console.log(target.request());
+}
+
+const target = new Target();
+clientCode(target);
+
+const adaptee = new Adaptee();
+console.log(`Adaptee ${adaptee.specificRequest()}`);
+
+const adapter = new Adapter(adaptee);
+clientCode(adapter);
