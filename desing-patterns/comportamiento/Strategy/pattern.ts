9c83f640ad533116e09ad70916bd14a780d8ca19
POXTRZ <148813649+POXTRZ@users.noreply.github.com>
2024-05-18 09:17:30 -0600

    Add files via upload

---
+++ desing-patterns/comportamiento/Strategy/pattern.ts
@@ -0,0 +1,68 @@
+interface TransportStrategy {
+  selectTransport(): void;
+}
+
+class Bus implements TransportStrategy {
+  selectTransport(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class Bike implements TransportStrategy {
+  selectTransport(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class Car implements TransportStrategy {
+  selectTransport(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class TransportContext {
+  transportStrategy: TransportStrategy;
+
+  constructor(strategy: TransportStrategy){
+    this.transportStrategy = strategy;
+  }
+
+  chooseTransport(): void {
+    this.transportStrategy.selectTransport();
+  }
+}
+
+class TransportMain {
+
+  static trasportSelected: TransportStrategy;
+
+  static main(mode: string): void {
+    const modes: { [key: string]: TransportStrategy } = {
+      'bus': new Bus(),
+      'bike': new Bike(),
+      'car': new Car(),
+    };
+
+    const modeSelected = modes[mode];
+
+    TransportMain.selectTransport(modeSelected);
+  }
+
+  static selectTransport(strategyMode: TransportStrategy) {
+    this.trasportSelected = strategyMode;
+    const transportContext = new TransportContext(strategyMode);
+    transportContext.chooseTransport();
+  }
+
+  static createRoute(a: string, b: string){
+    const transport = this.trasportSelected;
+
+   // calcular el destino con el transporte seleccionado
+  }
+
+}
+
+TransportMain.main('bus');
+TransportMain.createRoute('QRO', 'CDMX');
+
+
